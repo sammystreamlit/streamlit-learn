971259283298917398777df94680611b185207c1
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-03-18 16:10:21 -0400

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -7,6 +7,11 @@ from google_auth_oauthlib.flow import Flow
 import os
 from streamlit_elements import Elements
 import requests
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 # import google-api-python-client
 
 # # The code below is for the layout of the page
@@ -19,7 +24,67 @@ import requests
 #     layout=layout, page_title="Google Search Console Connector", page_icon="🔌"
 # )
 
+creds = {
+    "installed": {
+        "client_id": clientId,
+        "client_secret": clientSecret,
+        "redirect_uris": redirectUri,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+    }
+}
+
 # st.sidebar.image("logo.png", width=290)
+button2 = st.button("get email")
+
+if button2:
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+     try:
+        people = people_service.people().connections().list('people/me', personFields='names,emailAddresses')
+     except HttpError as err:
+        print(err)
+
+    # try:
+    #     service = build('people', 'v1', credentials=creds)
+
+    #     # Call the People API
+    #     print('List 10 connection names')
+    #     results = service.people().connections().list(
+    #         resourceName='people/me',
+    #         pageSize=10,
+    #         personFields='names,emailAddresses').execute()
+    #     connections = results.get('connections', [])
+
+    #     for person in connections:
+    #         names = person.get('names', [])
+    #         if names:
+    #             name = names[0].get('displayName')
+    #             print(name)
+    # except HttpError as err:
+    #     print(err)
+
+
+
 
 st.sidebar.markdown("")
 st.write("")
@@ -68,6 +133,28 @@ if button:
 #     else: 
 #         st.write(response)
 #     st.write(response.json())
+
+  def get_user_info(credentials):
+      """Send a request to the UserInfo API to retrieve the user's information.
+
+      Args:
+        credentials: oauth2client.client.OAuth2Credentials instance to authorize the
+                     request.
+      Returns:
+        User information as a dict.
+      """
+      user_info_service = build(
+          serviceName='oauth2', version='v2',
+          http=credentials.authorize(httplib2.Http()))
+      user_info = None
+      try:
+        user_info = user_info_service.userinfo().get().execute()
+      except errors.HttpError, e:
+        logging.error('An error occurred: %s', e)
+      if user_info and user_info.get('id'):
+        return user_info
+      else:
+        raise NoUserIdException()
 
 
 mt.button(
