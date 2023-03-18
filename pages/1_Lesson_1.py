5570b4b3707bee2529fd7f2c13b7daa0a84907f6
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-03-18 18:44:29 -0400

    Update 1_Lesson_1.py

--- pages/1_Lesson_1.py
+++ pages/1_Lesson_1.py
@@ -2,49 +2,52 @@ import streamlit as st
 import time
 import numpy as np
 from pathlib import Path
+from Intro import sidebar
 
 state=st.session_state
 
-with st.sidebar:
-    if state.logged_in and state.email:
-        logged_in_msg = "You're currently logged in as "+state.email
-        st.success(logged_in_msg, icon="✅")
-    else:
-        mt.button(
-            "Sign-in with Google",
-            target="_blank",
-            size="large",
-            variant="contained",
-            start_icon=mt.icons.exit_to_app,
-            onclick="login()",
-            style={"color": "#FFFFFF", "background": "#FF4B4B"},
-            href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
-            + clientId
-            + "&redirect_uri="
-            + redirectUri
-            + "&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent",
+sidebar()
 
-        )
+# with st.sidebar:
+#     if state.logged_in and state.email:
+#         logged_in_msg = "You're currently logged in as "+state.email
+#         st.success(logged_in_msg, icon="✅")
+#     else:
+#         mt.button(
+#             "Sign-in with Google",
+#             target="_blank",
+#             size="large",
+#             variant="contained",
+#             start_icon=mt.icons.exit_to_app,
+#             onclick="login()",
+#             style={"color": "#FFFFFF", "background": "#FF4B4B"},
+#             href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
+#             + clientId
+#             + "&redirect_uri="
+#             + redirectUri
+#             + "&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent",
 
-        mt.show(key="687")
+#         )
 
-        credentials = {
-            "installed": {
-                "client_id": clientId,
-                "client_secret": clientSecret,
-                "redirect_uris": redirectUri,
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://accounts.google.com/o/oauth2/token",
-            }
-        }
+#         mt.show(key="687")
 
-        flow = Flow.from_client_config(
-            credentials,
-            scopes=["https://www.googleapis.com/auth/webmasters.readonly"],
-            redirect_uri=redirectUri,
-        )
+#         credentials = {
+#             "installed": {
+#                 "client_id": clientId,
+#                 "client_secret": clientSecret,
+#                 "redirect_uris": redirectUri,
+#                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#                 "token_uri": "https://accounts.google.com/o/oauth2/token",
+#             }
+#         }
 
-        auth_url, _ = flow.authorization_url(prompt="consent")
+#         flow = Flow.from_client_config(
+#             credentials,
+#             scopes=["https://www.googleapis.com/auth/webmasters.readonly"],
+#             redirect_uri=redirectUri,
+#         )
+
+#         auth_url, _ = flow.authorization_url(prompt="consent")
 
 
 def read_markdown_file(markdown_file):
