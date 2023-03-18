f786cf89b718b86e16647432ffc31fb2ef8b77bc
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-03-18 18:17:15 -0400

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -22,30 +22,7 @@ redirectUri = st.secrets['REDIRECT_URI']
 # def login():
 #     st.write("called login fn")
 
-if st.experimental_get_query_params():
-    code = st.experimental_get_query_params()["code"][0]
 
-    # Get access token
-    url="https://accounts.google.com/o/oauth2/token"
-    body = {
-        "code": code,
-        "grant_type": "authorization_code",
-        "client_id": clientId,
-        "client_secret": clientSecret,
-        "redirect_uri": redirectUri,
-    }
-    r = requests.post(url, json=body)
-    response = json.loads(r.content)
-    state.access_token = response['access_token']
-    state.refresh_token = response['refresh_token']
-    state.id_token = response['id_token']
-
-    # Get current user's email address
-    url = "https://www.googleapis.com/oauth2/v3/userinfo?client_id="+clientId+"&client_secret="+clientSecret+"&redirect_uri="+redirectUri+"&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent"
-    r = requests.get(url,params={'access_token': state.access_token})
-    response = json.loads(r.content)
-    state.email = response['email']
-    st.write("You're currently logged in as "+state.email)
 
 # # The code below is for the layout of the page
 # if "widen" not in st.session_state:
@@ -273,3 +250,28 @@ with st.sidebar:
     )
 
     auth_url, _ = flow.authorization_url(prompt="consent")
+    
+    if st.experimental_get_query_params():
+        code = st.experimental_get_query_params()["code"][0]
+
+        # Get access token
+        url="https://accounts.google.com/o/oauth2/token"
+        body = {
+            "code": code,
+            "grant_type": "authorization_code",
+            "client_id": clientId,
+            "client_secret": clientSecret,
+            "redirect_uri": redirectUri,
+        }
+        r = requests.post(url, json=body)
+        response = json.loads(r.content)
+        state.access_token = response['access_token']
+        state.refresh_token = response['refresh_token']
+        state.id_token = response['id_token']
+
+        # Get current user's email address
+        url = "https://www.googleapis.com/oauth2/v3/userinfo?client_id="+clientId+"&client_secret="+clientSecret+"&redirect_uri="+redirectUri+"&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent"
+        r = requests.get(url,params={'access_token': state.access_token})
+        response = json.loads(r.content)
+        state.email = response['email']
+        st.write("You're currently logged in as "+state.email)
