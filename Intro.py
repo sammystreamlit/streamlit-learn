c02551770cdb2fbe7d5f64a78642198c6df07ed1
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-03-18 17:37:48 -0400

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -55,7 +55,10 @@ if button2:
     access_token = st.experimental_get_query_params()["code"][0]
     credentials = Credentials(None, client_id=clientId, client_secret=clientSecret)
     #    url = "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="+clientId+"&redirect_uri="+redirectUri+"&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent"
-    url = "https://www.googleapis.com/oauth2/v3/userinfo?response_type=code&client_id="+clientId+"&redirect_uri="+redirectUri+"&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent"
+    
+    # get access token
+#     url="accounts.google.com/o/oauth2/token&grant_type=authorization_code&client_id="+clientId+"&redirect_uri="+redirectUri+
+    url = "https://www.googleapis.com/oauth2/v3/userinfo?response_type=code&client_id="+clientId+"&client_secret="+clientSecret+"&redirect_uri="+redirectUri+"&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent"
     r = requests.get(url,params={'access_token': access_token})
     st.write(r.status_code)
     st.write(r.content)
