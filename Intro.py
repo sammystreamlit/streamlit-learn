8210ffeb59ef5b7cc44f2a0d65c14c63d2f511f8
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-03-18 16:58:28 -0400

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -56,8 +56,12 @@ if button2:
     credentials = Credentials(None, client_id=clientId, client_secret=clientSecret)
     service = build('people', 'v1', credentials=creds)
 #     results = service.people()
-    connections = service.people().connections().list(resourceName='people/me', personFields='names').execute()
-    st.write(connections)
+#     connections = service.people().connections().list(resourceName='people/me', personFields='names').execute()
+#     st.write(connections)
+    
+    people = service.people().connections().list('people/me', personFields='names,emailAddresses')
+    st.write(people)
+
 
 #     discovery.build("people", "v1", credentials = credentials)
     
