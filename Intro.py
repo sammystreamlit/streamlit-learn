8e7182d4d0574ccd7be57ba2be7cbe91b788e410
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-03-18 18:43:06 -0400

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -72,42 +72,45 @@ def check_if_logged_in():
 
 # st.sidebar.image("logo.png", width=290)
 
-with st.sidebar:
-    check_if_logged_in()
-    if not state.logged_in:
-        mt.button(
-            "Log in with Google",
-            target="_blank",
-            size="large",
-            variant="contained",
-#             start_icon=mt.icons.exit_to_app,
-            onclick="login()",
-            style={"color": "#FFFFFF", "background": "#FF4B4B"},
-            href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
-            + clientId
-            + "&redirect_uri="
-            + redirectUri
-            + "&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent",
-        )
+def sidebar():
+    with st.sidebar:
+        check_if_logged_in()
+        if not state.logged_in:
+            mt.button(
+                "Log in with Google",
+                target="_blank",
+                size="large",
+                variant="contained",
+    #             start_icon=mt.icons.exit_to_app,
+                onclick="login()",
+                style={"color": "#FFFFFF", "background": "#FF4B4B"},
+                href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
+                + clientId
+                + "&redirect_uri="
+                + redirectUri
+                + "&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent",
+            )
 
-        mt.show(key="687")
+            mt.show(key="687")
 
-        credentials = {
-            "installed": {
-                "client_id": clientId,
-                "client_secret": clientSecret,
-                "redirect_uris": redirectUri,
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://accounts.google.com/o/oauth2/token",
+            credentials = {
+                "installed": {
+                    "client_id": clientId,
+                    "client_secret": clientSecret,
+                    "redirect_uris": redirectUri,
+                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri": "https://accounts.google.com/o/oauth2/token",
+                }
             }
-        }
 
-        flow = Flow.from_client_config(
-            credentials,
-            scopes=["https://www.googleapis.com/auth/webmasters.readonly"],
-            redirect_uri=redirectUri,
-        )
+            flow = Flow.from_client_config(
+                credentials,
+                scopes=["https://www.googleapis.com/auth/webmasters.readonly"],
+                redirect_uri=redirectUri,
+            )
 
-        auth_url, _ = flow.authorization_url(prompt="consent")
-    else:
-        get_email()
+            auth_url, _ = flow.authorization_url(prompt="consent")
+        else:
+            get_email()
+            
+sidebar()
